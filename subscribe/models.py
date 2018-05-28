2ec6bb368407f6336a412caa395507117d039de3
Ach1916 <glloydiii@gmail.com>
2018-05-28 15:06:14 -0500

    Update models.py
    Now with a little knowledge of django, Ive added an actually model. Each instance of the model now has attributes to go alone with it

--- subscribe/models.py
+++ subscribe/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
-# Create your models here.
-Doctors_list = []
-Dentist_list = []
+
+class Contacts (models.Model):
+    customer_number = models.CharField(max_length=15)
+    customer_age = models.CharField(max_length=4,
+                                    null=True)
+    customer_income = models.CharField(max_length=10,
+                                       null=True)
+
+    def __unicode__(self):
+        return self.customer_number
