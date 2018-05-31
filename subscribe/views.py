21a5bcbf9ca39546a8cce065196a4aeb4ebc927d
Ach1916 <glloydiii@gmail.com>
2018-05-30 21:38:02 -0500

    Update views.py

--- subscribe/views.py
+++ subscribe/views.py
@@ -9,13 +9,34 @@ def sms_choice(request):
     twilio_request = decompose(request)
     contact_num = twilio_request.from_
     response = twilio_request.body
-    contact_info = ['Thanks for your subscription',
-                    "How old are you?", "Annual Income?"]
     resp = MessagingResponse()
+    contact_info = ['Thanks for your subscription, How old are you?',
+                    'What is your annual income?',
+                    'Do you have a vehicle?']
+
+    # 'Create Contacts instance'
     contact, created = Contacts.objects.get_or_create(customer_number=contact_num)
+
+    # 'Send and record last sent sms'
+
     if created:
         resp.message(contact_info[0])
+        contact.last_sms = '0'
+        contact.save()
+
+    elif not created:
+        if contact.last_sms == '0':
+            resp.message(contact_info[1])
+            contact.last_sms = '1'
+            contact.save()
+
+        elif contact.last_sms == '1':
+                resp.message(contact_info[2])
+                contact.last_sms = '2'
+                contact.save()
+
     else:
-        resp.message(contact_info[1])
+        resp.message('No record found')
+
     print(contact_num, response)
     return resp
