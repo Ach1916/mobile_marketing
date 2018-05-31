486768d16573886fefec146207e49c7a41c070a8
Ach1916 <glloydiii@gmail.com>
2018-05-30 21:39:30 -0500

    Update models.py

--- subscribe/models.py
+++ subscribe/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Contacts (models.Model):
     customer_number = models.CharField(max_length=15)
+    last_sms = models.CharField(max_length=10,
+                                         null=True)
     customer_age = models.CharField(max_length=4,
                                     null=True)
     customer_income = models.CharField(max_length=10,
